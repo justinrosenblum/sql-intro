430e0e4f2976c52ae3f8b01b300cb36b846319f1
justinrosenblum <Justin.Rosenblum@kellogg.northwestern.edu>
2022-04-07 01:55:57 +0000

    completed labs 2 and 3

--- lab-3-3.sql
+++ lab-3-3.sql
@@ -15,4 +15,5 @@
 -- | 2019 | Houston Astros                | 107       |
 -- | 2004 | St. Louis Cardinals           | 105       |
 
+SELECT year, name, MAX(wins) FROM teams WHERE year >= 1960 GROUP BY wins ORDER BY wins DESC;
 
