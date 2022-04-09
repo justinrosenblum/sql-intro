50057e3bb53ba3c59e925de94db1c4c26b88031d
justinrosenblum <Justin.Rosenblum@kellogg.northwestern.edu>
2022-04-09 01:59:58 +0000

    completed lab 4

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -3,8 +3,12 @@
 -- Expected result:
 -- 2935
 
-SELECT players.first_name, players.last_name FROM players
+SELECT first_name, last_name, id FROM players
 WHERE first_name = "Barry" AND last_name = "Bonds";
---FROM players
---INNER JOIN stats ON stats.player_id = player.id
---WHERE first_name = "Barry" AND last_name = "Bonds";
+
+SELECT player_id, SUM(hits) FROM stats
+WHERE player_id = 1678;
+
+SELECT SUM(stats.hits)
+FROM players INNER JOIN stats ON players.id = stats.player_id
+WHERE players.first_name = "Barry" AND players.last_name = "Bonds";
